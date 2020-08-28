0078caf6aa2946bae7b682ac6e4e2b1432f47b71
pausun <pfsun9999@msn.com>
2020-08-28 00:32:32 -0800

    Worked on functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var result = document.getElementById("results");
 var finalScore = document.getElementById("final");
 var gameOver = document.getElementById("gameover");
 var quizQuestions = document.getElementById("questions");
-var timer = document.getElementById("timer");
+var quizTimer = document.getElementById("timer");
 var startButton = document.getElementById("start");
 var startQuiz = document.getElementById("home");
 var scoreContainer = document.getElementById("scoreContainer");
@@ -23,27 +23,27 @@ var buttonD = document.getElementById("d");
 var questions = [
     {
         question: "Where am I?",
-        choice1: "I don't know",
-        choice2: "Who are you?",
-        choice3: "I don't care.",
-        choice4: "You're lost.",
-        answer: 4,
+        choiceA: "I don't know",
+        choiceB: "Who are you?",
+        choiceC: "I don't care.",
+        choiceD: "You're lost.",
+        correctAnswer: "d"
     },
     {
         question: "Why am I hungry?",
-        choice1: "Please don't eat me.",
-        choice2: "You forgot to have lunch.",
-        choice3: "You exercise so much, it doesn't matter how much you eat. It will never be enough. You're cursed.",
-        choice4: "Because I am eating in front of you and you have nothing.",
-        answer: 1,
+        choiceA: "Please don't eat me.",
+        choiceB: "You forgot to have lunch.",
+        choiceC: "You exercise so much, it doesn't matter how much you eat. It will never be enough. You're cursed.",
+        choiceD: "Because I am eating in front of you and you have nothing.",
+        correctAnswer: "a"
     },
     {
         question: "What do you do when you see that your friend is upset?",
-        choice1: "Run away as fast and as far as possible.",
-        choice2: "Use a friend who gives good advice as a human shield.",
-        choice3: "Distract him or her with candy or other desirable objects.",
-        choice4: "Help the friend calm down and find a solution.",
-        answer: 2,
+        choiceA: "Run away as fast and as far as possible.",
+        choiceB: "Use a friend who gives good advice as a human shield.",
+        choiceC: "Distract him or her with candy or other desirable objects.",
+        choiceD: "Help the friend calm down and find a solution.",
+        correctAnswer: "b"
     },
 ];
 
@@ -56,15 +56,94 @@ var timer;
 var score = 0;
 var correct;
 
-function highScores() {
-
+function generateQuestions() {
+    gameOver.style.display = "none";
+    if (questionsIndex === totalQuestions) {
+        return yourScore;
+    }
+    var yourQuestion = questions[questionsIndex];
+    quizQuestions.innerHTML = "<p>" + yourQuestion.question + "</p>";
+    buttonA.innerHTML = yourQuestion.choiceA;
+    buttonB.innerHTML = yourQuestion.choiceB;
+    buttonC.innerHTML = yourQuestion.choiceC;
+    buttonD.innerHTML = yourQuestion.choiceD;
 }
 
-function checkAnswer() {
+// Starts the quiz
+function start() {
+    gameOver.style.display = "none";
+    startQuiz.style.display = "none";
+    generateQuestions();
 
+    // Countdown
+    timer = setInterval(function() {
+        remainingTime--;
+        quizTimer.textContent = "Remaining Time: " + remainingTime;
+        if (remainingTime === 0) {
+            clearInterval(timer);
+            yourScore();
+        }
+    }, 1000);
+    quiz.style.display = "block";
 }
 
-function retake() {
+// Displays your score
+function yourScore() {
+    quiz.style.display = "none"
+    gameOver.style.display = "flex";
+    clearInterval(timer);
+    initials.value = "";
+    finalScore.innerHTML = "Your score: " + score + " / " + questions.length;
+};
+
+submitScore.addEventListener("click", function highScores() {
+    if (initials.value === "") {
+        alert("Please enter your initials.");
+        return false;
+    } else {
+        var savedScores = JSON.parse(localStorage.getItem("savedScores")) || [];
+        var player = initials.value.trim();
+        var currentScore = {
+            name : player,
+            score : score
+        };
+
+        gameOver.style.display = "none";
+        scoreContainer.style.display = "flex";
+        scorePage.style.display = "block";
+        endGame.style.display = "flex";
+
+        savedScores.push(currentScore);
+        localStorage.setItem("savedScores", JSON.stringify(savedScores));
+        generateScores();
+    }
+});
+
+function generateScores(){
+    displayName.innerHTML = "";
+    displayScore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedScores")) || [];
+    for (i = 0; i < highscores.length; i++){
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        displayName.appendChild(newNameSpan);
+        displayScore.appendChild(newScoreSpan);
+    }
+}
+
+function showHighscores(){
+    startQuiz.style.display = "none"
+    gameOver.style.display = "none";
+    scoreContainer.style.display = "flex";
+    scorePage.style.display = "block";
+    endGame.style.display = "flex";
+
+    generateScores();
+}
+
+function retakeQuiz() {
     scoreContainer.style.display = "none";
     gameOver.style.display = "none";
     startQuiz.style.display = "flex";
@@ -73,8 +152,26 @@ function retake() {
     currentQuestionIndex = 0;
 }
 
-function clear() {
+function clearAll() {
     window.localStorage.clear();
     displayName.textContent = "";
     displayScore.textContent = "";
 }
+
+function checkAnswer(answer) {
+    correct = questions[questionsIndex].correctAnswer;
+    if (answer === correct && questionsIndex !== totalQuestions) {
+        score++;
+        alert("Correct");
+        questionsIndex++;
+        generateQuestions();
+    } else if (answer !== correct && questionsIndex !== totalQuestions) {
+        alert("Incorrect");
+        questionsIndex++;
+        generateQuestions();
+    } else {
+        yourScore();
+    }
+}
+
+startButton.addEventListener("click", start);
