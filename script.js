0983d52c139a1a6472007475ec5d1fdcbcb9b34f
pausun <pfsun9999@msn.com>
2020-08-27 17:31:07 -0800

    Bug fixes

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const question = document.getElementById("question");
 
-// defines variable choices as an array using class choice-text from questions.html as the elements.
+// Defines variable choices as an array using class choice-text from questions.html as the elements.
 
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+console.log(choices);
+
+// Defines more variables
 
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let emptyQuestions = [];
+let availableQuestions = [];
+
+// Defines array of hard-coded questions
 
 let questions = [
     {
@@ -34,44 +39,46 @@ let questions = [
         choice3: "Distract him or her with candy or other desirable objects.",
         choice4: "Help the friend calm down and find a solution.",
         answer: 2,
-    }
-]
+    },
+];
 
+const perfectBonus = 10;
 const totalQuestions = 3;
 
-function gameStart() {
+gameStart = () => {
     questionCounter = 0;
     score = 0;
-    emptyQuestions = [...questions];
-    next();
+    availableQuestions = [...questions];
+    nextQuestion();
 };
 
-function next() {
-    if(emptyQuestions.length === 0 || questionCounter >= totalQuestions) {
-        return window.location.assign("/end.html");
+nextQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= totalQuestions) {  
+        // Takes user to game over page if there are no more questions left.
+        return window.location.assign("/gameover.html");
     }
     questionCounter++;
-    const questionIndex = Math.floor(Math.random() * emptyQuestions.length);
-    next = emptyQuestions[questionIndex];
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
     choices.forEach((choice) => {
         const number = choice.dataset["number"];
-        choice.innterText = next["choice" + number];
+        choice.innerText = currentQuestion["choice" + number];
     });
-    emptyQuestions.splice(questionIndex, 1);
+    availableQuestions.splice(questionIndex, 1);
 
     acceptingAnswers = true;
 };
 
 choices.forEach((choice) => {
     choice.addEventListener("click", (e) => {
-        if(!acceptingAnswers) return;
+        if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
-        next();
+        nextQuestion();
     });
 });
 
